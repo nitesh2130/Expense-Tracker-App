0b864ce5b9fad6a2c26dfdbb809aef4fb7a46246
Nitesh Sharma <niteshsharma21307665@gmail.com>
2025-02-11 18:06:26 +0530

    This is make Register page and Login page for the user , user can be move login page to register page and also do reverse it.

--- frontend/Expense_tracker_frontend/src/main.tsx
+++ frontend/Expense_tracker_frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+import App from "./App.tsx";
+import React from "react";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
